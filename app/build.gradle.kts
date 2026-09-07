a916ef33049840f00f22c2872bcb1d67ade93217
aldanaluis2424-create <aldanaluis2424@gmail.com>
2026-09-07 16:07:05 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.aistudio.torahia.kxpq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "1.0.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
